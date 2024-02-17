5cd58a3eb9e8e5d7cc0099ad3e752f27b28f3951
Jannik <53686562+evolving-dev@users.noreply.github.com>
2024-02-17 14:37:48 +0100

    All photons now have the same speed

--- Photon.py
+++ Photon.py
@@ -1,11 +1,12 @@
+import math
 import random
 
 class Photon:
     def __init__(self, x, y, speed_factor = 1):
-        speed_factor = 8 * speed_factor #Default speed factor: 8
+        speed_factor = 4 * speed_factor #Default speed factor: 4
 
         self.x = x
         self.y = y
 
-        self.vx = (random.random() - 0.5) * speed_factor
-        self.vy = (random.random() - 0.5) * speed_factor
+        self.vx = (random.random() - 0.5) * speed_factor * 2
+        self.vy = random.choice([-1, 1]) * math.sqrt(speed_factor ** 2 - self.vx ** 2) #Ensures equal speeds for all photons
