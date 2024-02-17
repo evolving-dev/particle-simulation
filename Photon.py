e78c758d82b46ec95aabf1c9c6809723ec5f98e8
Jannik <53686562+evolving-dev@users.noreply.github.com>
2024-02-17 14:28:22 +0100

    Rebalancing

--- Photon.py
+++ Photon.py
@@ -2,7 +2,7 @@ import random
 
 class Photon:
     def __init__(self, x, y, speed_factor = 1):
-        speed_factor = 4 * speed_factor #Default speed factor: 4
+        speed_factor = 8 * speed_factor #Default speed factor: 8
 
         self.x = x
         self.y = y
